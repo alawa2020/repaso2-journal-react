33dc6c92c419f67e6886e57cd575ce986c49eeaa
alawa20 <huder13168@gmail.com>
2022-03-12 08:02:46 -0500

    diseño journal

--- src/components/journal/JournalScreen.js
+++ src/components/journal/JournalScreen.js
@@ -1,20 +1,24 @@
-import { useDispatch, useSelector } from 'react-redux';
-import { startAuthSignOut } from '../../actions/authActions';
+import React from 'react';
+import { Sidebar } from './Sidebar';
+import { NoteScreen } from '../notes/NoteScreen';
+// import { NothingSelected } from './NothingSelected';
+
 
 export const JournalScreen = () => {
-  //hooks
-  const dispatch = useDispatch();
-  const { name } = useSelector((state) => state.auth);
-  const handleSignOut = () => {
-    dispatch(startAuthSignOut());
-  };
-  return (
-    <div>
-      <h1>Welcome: {name}</h1>
-      <h2>website under development...</h2>
-      <button type="button" onClick={handleSignOut}>
-        Logout
-      </button>
-    </div>
-  );
-};
+    return (
+        <div className="journal__main-content">
+            
+            <Sidebar />
+
+
+            <main>
+
+                {/* <NothingSelected /> */}
+                <NoteScreen />
+
+            </main>
+
+
+        </div>
+    )
+}
