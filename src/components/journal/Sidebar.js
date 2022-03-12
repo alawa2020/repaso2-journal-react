f837adc59f72316b8a69c750b43805e890b31c58
alawa20 <huder13168@gmail.com>
2022-03-12 08:26:05 -0500

    logout and some refactoring

--- src/components/journal/Sidebar.js
+++ src/components/journal/Sidebar.js
@@ -1,30 +1,36 @@
-import React from 'react'
-import { JournalEntries } from './JournalEntries'
+import { useDispatch, useSelector } from 'react-redux';
+import { startAuthSignOut } from '../../actions/authActions';
+import { JournalEntries } from './JournalEntries';
 
 export const Sidebar = () => {
-    return (
-        <aside className="journal__sidebar">
-            
-            <div className="journal__sidebar-navbar">
-                <h3 className="mt-5">
-                    <i className="far fa-moon"></i>
-                    <span> Fernando</span>
-                </h3>
+  //hooks
+  const { name } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
 
-                <button className="btn">
-                    Logout
-                </button>
-            </div>
+  //functions
+  const handleSignOut = () => {
+    dispatch(startAuthSignOut());
+  };
 
-            <div className="journal__new-entry">
-                <i className="far fa-calendar-plus fa-5x"></i>
-                <p className="mt-5">
-                    New entry
-                </p>
-            </div>
+  return (
+    <aside className="journal__sidebar">
+      <div className="journal__sidebar-navbar">
+        <h3 className="mt-5">
+          <i className="far fa-moon"></i>
+          <span> {name} </span>
+        </h3>
 
-            <JournalEntries />    
+        <button onClick={handleSignOut} className="btn2">
+          Logout
+        </button>
+      </div>
 
-        </aside>
-    )
-}
+      <div className="journal__new-entry">
+        <i className="far fa-calendar-plus fa-5x"></i>
+        <p className="mt-5">New entry</p>
+      </div>
+
+      <JournalEntries />
+    </aside>
+  );
+};
