3753598c830795c731733219823fd3be3c15b0fa
alawa20 <huder13168@gmail.com>
2022-03-11 22:53:52 -0500

    check if there a user

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,12 +1,39 @@
-import React, { useState } from 'react';
-import { Redirect } from 'react-router-dom';
-import { BrowserRouter as Router, Switch } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+
+import { BrowserRouter as Router, Switch, Redirect } from 'react-router-dom';
+import { onAuthStateChanged } from 'firebase/auth';
+
+import { auth } from '../firebase/config';
 import { JournalScreen } from '../components/journal/JournalScreen';
 import { AuthRouter } from './AuthRouter';
 import { PrivateRoute } from './PrivateRoute';
 import { PublicRoute } from './PublicRoute';
+import { useDispatch } from 'react-redux';
+import { doAuthLogin } from '../actions/authActions';
+
 export const AppRouter = () => {
+  //hooks
   const [isAuth, setIsAuth] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user?.uid) {
+        setIsAuth(true);
+        dispatch(doAuthLogin(user.uid, user.displayName));
+      } else {
+        setIsAuth(false);
+      }
+      setLoading(false);
+    });
+  }, [dispatch]);
+
+  //otros
+
+  if (loading) {
+    return <h1> Loading...</h1>;
+  }
   return (
     <Router>
       <Switch>
